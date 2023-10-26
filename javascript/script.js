aafd61bdb42615d0cca2963b494eab379506a149
michaelmenesi <menesimichael61@gmail.com>
2023-10-26 18:38:22 +0100

    dashboard responsive

--- javascript/script.js
+++ javascript/script.js
@@ -68,12 +68,26 @@ ScrollReveal().reveal(".blog-container", {delay:200, easing:"ease-out", interval
 
 // scroll reveal ends 
 
-// sticky nav bar
+// contact form js
 
-const navbar = document.querySelector(".navbar")
-
-
-window.onscroll = ()=>{
-    this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
+const inputs = document.querySelectorAll(".input");
+             
+function focusFunc(){
+    let parent = this.parentNode;
+    parent.classList.add("focus");
 }
-// sticky nav bar ends
+ 
+function blurFunc(){
+    let parent = this.parentNode;
+    if(this.value == "") {
+        parent.classList.remove("focus");
+    }
+    
+}
+ 
+inputs.forEach(input =>{
+    input.addEventListener("focus", focusFunc);
+    input.addEventListener("blur", focusFunc);
+})
+
+// contact form js ends
