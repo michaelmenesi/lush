fc3b602943147b41193f66e5882739d0a482ae15
michaelmenesi <menesimichael61@gmail.com>
2024-01-12 07:22:48 +0100

    slider update

--- javascript/script.js
+++ javascript/script.js
@@ -91,3 +91,27 @@ inputs.forEach(input =>{
 })
 
 // contact form js ends
+
+// slider 
+var TrandingSlider = new Swiper('.tranding-slider', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    loop: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+      rotate: 0,
+      stretch: 0,
+      depth: 100,
+      modifier: 2.5,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
+  });
+// slider ends
